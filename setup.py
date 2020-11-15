fdf07e474bb1be2e48a0b6939e6baa276f50d4df
Cole Wilson <cole@colewilson.xyz>
2020-11-15 19:56:36 +0000

    Updated to v0.0.2

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="nought", # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
 		scripts=['bin/nought'],
     author="Cole Wilson",
     author_email="cole@colewilson.xyz",
-    description="A small example package",
+    description="A super customizable file cleaner/organizer/automator",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/cole-wilson/nought",
@@ -19,8 +19,7 @@ setuptools.setup(
 		],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "License :: OSI Approved :: Apache 2.0 License",
     ],
     python_requires='>=3.6',
 )
